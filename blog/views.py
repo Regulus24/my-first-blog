de7913dbb9bf0b6ec162d3cb014937c2f4927b62
Regulus <Regulucio@gmail.com>
2021-03-02 15:56:39 +0800

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,7 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})	
-
+    return render(request, '/post_list.html', {'posts': posts})	
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date'
 
 	
